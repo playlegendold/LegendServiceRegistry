49fd339050a27c017c9ddb1b4a487876c2aee5f5
Kroseida <leitung@nationwar.de>
2020-01-19 23:03:25 +0100

    added basic service implementation

--- bukkit/src/main/java/net/playlegend/legendserviceregistry/bukkit/LegendBukkitPlugin.java
+++ bukkit/src/main/java/net/playlegend/legendserviceregistry/bukkit/LegendBukkitPlugin.java
@@ -1,7 +1,13 @@
 package net.playlegend.legendserviceregistry.bukkit;
 
+import lombok.Getter;
+import net.playlegend.legendserviceregistry.common.ServiceAccessor;
 import net.playlegend.legendserviceregistry.common.ServiceAccessorOwner;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
 public abstract class LegendBukkitPlugin extends JavaPlugin implements ServiceAccessorOwner {
+
+    private final ServiceAccessor accessor = new ServiceAccessor(this);
+
 }
