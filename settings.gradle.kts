7383c03aa4958f78f00c6cffdb1df57591b13b8f
Kroseida <kroseida@gmail.com>
2020-01-22 11:32:40 +0100

    removed settings.gradle.kts from modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,7 @@ rootProject.name = "legendserviceregistry"
 include("common")
 include("proxy")
 include("bukkit")
+
+project(":common").name = "legendserviceregistry-common"
+project(":proxy").name = "legendserviceregistry-proxy"
+project(":bukkit").name = "legendserviceregistry-bukkit"
