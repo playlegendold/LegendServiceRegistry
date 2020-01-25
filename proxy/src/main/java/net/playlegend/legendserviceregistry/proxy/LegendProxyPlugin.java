11f01c9316a911dad362290655b7e6d93b6988e4
Kroseida <leitung@nationwar.de>
2020-01-25 17:38:15 +0100

    fixed missing accessor in proxy plugin

--- proxy/src/main/java/net/playlegend/legendserviceregistry/proxy/LegendProxyPlugin.java
+++ proxy/src/main/java/net/playlegend/legendserviceregistry/proxy/LegendProxyPlugin.java
@@ -1,9 +1,14 @@
 package net.playlegend.legendserviceregistry.proxy;
 
+import lombok.Getter;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.playlegend.legendserviceregistry.common.ServiceAccessor;
 import net.playlegend.legendserviceregistry.common.ServiceAccessorOwner;
 
 public abstract class LegendProxyPlugin extends Plugin implements ServiceAccessorOwner {
+
+    @Getter
+    private final ServiceAccessor accessor = new ServiceAccessor(this);
 
     @Override
     public String getName() {
