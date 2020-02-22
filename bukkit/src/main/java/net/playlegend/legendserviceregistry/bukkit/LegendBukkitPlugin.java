5e9c46200c7cd85537b52ec37cbad4229b7151f0
Lukas Jost <lukas.jost@outlook.com>
2020-02-22 17:56:55 +0100

    added java docs

--- bukkit/src/main/java/net/playlegend/legendserviceregistry/bukkit/LegendBukkitPlugin.java
+++ bukkit/src/main/java/net/playlegend/legendserviceregistry/bukkit/LegendBukkitPlugin.java
@@ -5,9 +5,15 @@ import net.playlegend.legendserviceregistry.common.ServiceAccessor;
 import net.playlegend.legendserviceregistry.common.ServiceAccessorOwner;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * {@link LegendBukkitPlugin} should be extended in every bukkit plugin.
+ */
 @Getter
 public abstract class LegendBukkitPlugin extends JavaPlugin implements ServiceAccessorOwner {
 
+    /**
+     * Plugin specific {@link ServiceAccessor}.
+     */
     private final ServiceAccessor accessor = new ServiceAccessor(this);
 
 }
--- common/src/main/java/net/playlegend/legendserviceregistry/common/ServiceAccessorOwner.java
+++ common/src/main/java/net/playlegend/legendserviceregistry/common/ServiceAccessorOwner.java
@@ -1,7 +1,14 @@
 package net.playlegend.legendserviceregistry.common;
 
+/**
+ * This interface represents a {@link ServiceAccessorOwner}.
+ */
 public interface ServiceAccessorOwner {
 
+    /**
+     * Get the name of the {@link ServiceAccessorOwner}.
+     * @return name.
+     */
     String getName();
 
 }
