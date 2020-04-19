7478313a4d48a88be631eb264b1c71d90120e298
Kroseida <leitung@nationwar.de>
2020-04-19 18:57:56 +0200

    implemented lazyloading feature

---
+++ common/src/main/java/net/playlegend/legendserviceregistry/common/LazyLoad.java
@@ -0,0 +1,23 @@
+package net.playlegend.legendserviceregistry.common;
+
+public class LazyLoad<T extends Service> {
+
+  private final Class<T> serviceName;
+  private final ServiceAccessor accessor;
+  private T service;
+
+  public LazyLoad(final Class<T> serviceName, final ServiceAccessor accessor) {
+    this.serviceName = serviceName;
+    this.accessor = accessor;
+  }
+
+  public T get() {
+    if (service == null) {
+      service = accessor.access(serviceName);
+    }
+    if (service == null) {
+      throw new NullPointerException("Service is not loaded yet!");
+    }
+    return service;
+  }
+}
