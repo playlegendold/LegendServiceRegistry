b30f503784076d4f971cb8393368254a8249be60
Lukas Jost <lukas.jost@outlook.com>
2020-05-13 18:05:49 +0200

    updated repository url (#7)
    * updated repository url
    upgraded gradle to 6.4
    
    Signed-off-by: Lukas Jost <lukas.jost@outlook.com>
    
    * removed sonarqube dependent actions
    
    Signed-off-by: Lukas Jost <lukas.jost@outlook.com>

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -43,7 +43,7 @@ publishing {
                 username = System.getenv("repositoryUser")
                 password = System.getenv("repositoryPassword")
             }
-            url = uri("https://repository.playlegend.dev/legend-internal")
+            url = uri("https://repository.playlegend.net/artifactory/legend")
         }
     }
 }
