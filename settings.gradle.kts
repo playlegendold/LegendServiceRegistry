19f22b79767353d410a5a09cf527d1397c0d14b5
Kevin Huber <ItsKev@users.noreply.github.com>
2021-01-10 16:55:22 +0100

    Changed pre-push git hook (#25)
    * Changed pre-push git hook
    
    * added missing enterprise plugin
    
    Co-authored-by: Lukas Jost <lukas.jost@outlook.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,28 @@ include("bukkit")
 project(":common").name = "LegendServiceRegistry-Common"
 project(":proxy").name = "LegendServiceRegistry-Proxy"
 project(":bukkit").name = "LegendServiceRegistry-Bukkit"
+
+plugins {
+    id("com.gradle.enterprise").version("3.5.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = false
+    }
+    remote(HttpBuildCache::class) {
+        url = uri("https://repository.playlegend.net/artifactory/cache/")
+        credentials {
+            username = System.getenv("repositoryUser")
+            password = System.getenv("repositoryPassword")
+        }
+        isPush = true;
+    }
+}
